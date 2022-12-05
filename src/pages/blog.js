022e88527245aad31c60546504562b87de69c733
Daf <dafne.azzolina@gmail.com>
2022-12-05 18:31:34 -0300

    Create blog posts

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -6,21 +6,30 @@ import Seo from '../components/seo'
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
-    <ul>
-      {data.allFile.nodes.map((node) => <li key={node.name}>{node.name}</li>)}
-    </ul>
+      {data.allMdx.nodes.map((node) =>
+      <article key={node.id}>
+        <h2>{node.frontmatter.title}</h2>
+        <p>Posted: {node.frontmatter.date}</p>
+        <p>{node.excerpt}</p>
+      </article> 
+      )}
     </Layout>
   )
 }
 
 export const query = graphql`
-  query {
-    allFile {
-      nodes {
-        name
+query {
+  allMdx(sort: {frontmatter: {date: DESC}}) {
+    nodes {
+      frontmatter {
+        title
+        date(formatString: "D MMMM, YYYY")
       }
+      id
+      excerpt
     }
   }
+}
 `
 
 export const Head = () => <Seo title="My Blog Posts" />
