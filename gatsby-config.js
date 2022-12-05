84227c56c998fb643e5a0f8d1b367aef290ea4a6
Daf <dafne.azzolina@gmail.com>
2022-12-05 19:28:06 -0300

    Add images to blog posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,5 +16,6 @@ module.exports = {
       }
     },
     "gatsby-plugin-mdx",
+    "gatsby-transformer-sharp",
   ],
 }
--- src/pages/blog/{mdx.frontmatter__slug}.js
+++ src/pages/blog/{mdx.frontmatter__slug}.js
@@ -1,27 +1,44 @@
-import * as React from 'react'
-import { graphql } from 'gatsby'
-import Layout from '../../components/layout'
-import Seo from '../../components/seo'
+import * as React from "react";
+import { graphql } from "gatsby";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import Layout from "../../components/layout";
+import Seo from "../../components/seo";
 
 const BlogPost = ({ data, children }) => {
+  const image = getImage(data.mdx.frontmatter.hero_image);
   return (
     <Layout pageTitle={data.mdx.frontmatter.title}>
-      <p>{data.mdx.frontmatter.date}</p>
+      <p>Posted: {data.mdx.frontmatter.date}</p>
+      <GatsbyImage image={image} alt={data.mdx.frontmatter.hero_image_alt} />
+      <p>
+        Photo Credit:{" "}
+        <a href={data.mdx.frontmatter.hero_image_credit_link}>
+          {data.mdx.frontmatter.hero_image_credit_text}
+        </a>
+      </p>
       {children}
     </Layout>
-  )
-}
+  );
+};
 export const query = graphql`
-query ($id: String) {
-  mdx(id: {eq: $id}) {
-    frontmatter {
-      date(formatString: "D MMMM, YYYY")
-      title
+  query ($id: String) {
+    mdx(id: { eq: $id }) {
+      frontmatter {
+        title
+        date(formatString: "D MMMM, YYYY")
+        hero_image_alt
+        hero_image_credit_link
+        hero_image_credit_text
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
+      }
     }
   }
-}
-`
+`;
 
-export const Head = ({ data }) => <Seo title={data.mdx.frontmatter.title} />
+export const Head = ({ data }) => <Seo title={data.mdx.frontmatter.title} />;
 
-export default BlogPost
+export default BlogPost;
